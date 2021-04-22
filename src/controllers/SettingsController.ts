93580a03043f53c0ff9c985c06cb646bb986c330
jeffersoncharlles <contato@jefferdeveloper.com>
2021-04-21 21:37:49 -0300

    primary error msg commit

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -13,9 +13,18 @@ class SettingsController{
 
         const settingsServices = new SettingsServices();
 
-        const settings = await settingsServices.create({chat,username});
 
-        return res.json(settings);
+        //para tratar try catch
+        try {
+            const settings = await settingsServices.create({chat,username});
+
+            return res.json(settings);
+        } catch (error) {
+            //retornar erros se nao de certo 
+            return res.status(400).json({
+                message: error.message
+            })
+        }
     }
 /*=====================================================================*/
 /*=====================================================================*/
