f65ce78cb001db9d7851488303f7c5adcf81a807
jeffersoncharlles <contato@jefferdeveloper.com>
2021-04-24 00:13:42 -0300

    create connection tables & save connection commit

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -1,9 +1,35 @@
 import {io} from '../http';
+import {ConnectionsServices} from '../services/ConnectionsServices';
+import { UsersServices } from '../services/UsersServices';
 
 
 io.on("connection",(socket)=>{
-    socket.on('client_first_access', params =>{
+
+    const connectionsServices = new ConnectionsServices();
+    const userServices = new UsersServices();
+
+    socket.on('client_first_access', async params =>{
+        const socket_id = socket.id;
         // console.log(params);
+
+        const {text, email} = params;
+
         //Salvar a conexao com o socket_id, user_id
-    })
+        const userExists = await userServices.findByEmail(email);
+
+        if (!userExists) {
+            const user = await userServices.create(email);
+
+            await connectionsServices.create({
+                socket_id,
+                user_id: user.id
+            });
+        }else{
+            await connectionsServices.create({
+                socket_id,
+                user_id: userExists.id
+            });
+        }
+
+    });
 });
